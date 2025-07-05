71041ac8ddb70107131850da942d1dae0a9a51b7
Tufan <dtufan38@gmail.com>
2025-07-05 13:05:07 +0530

    creating a serializer for Employees

--- api/serializers.py
+++ api/serializers.py
@@ -3,12 +3,20 @@ from students.models import Students
 from employees.models import Employees
 
 
+# Importing Django REST Framework's ModelSerializer to easily convert model instances to JSON and vice versa
+from rest_framework import serializers
+
+# Serializer for the Students model
+# This will handle converting Student model objects into JSON format and validating input data
 class StudentSerializer(serializers.ModelSerializer):
     class Meta:
-        model = Students
-        fields = "__all__"
+        model = Students  # Tells the serializer which model to base the serialization on
+        fields = "__all__"  # Automatically include all fields from the Students model
 
-class EmoloyeeSerializer(serializers.ModelSerializer):
+# Serializer for the Employees model
+# Same concept as above, but applied to a different model (Employees)
+class EmoloyeeSerializer(serializers.ModelSerializer):  # typo in class name: should be EmployeeSerializer
     class Meta:
-        model = Employees
-        fields = "__all__"
+        model = Employees  # Specify the model to serialize
+        fields = "__all__"  # Include every field from the Employees model
+
