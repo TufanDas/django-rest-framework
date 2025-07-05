1555529b19dd680091107d72a35d49be96dc1ff4
Tufan <dtufan38@gmail.com>
2025-07-05 13:03:59 +0530

    creating a serializer for Employees

--- api/serializers.py
+++ api/serializers.py
@@ -1,8 +1,14 @@
 from rest_framework import serializers
 from students.models import Students
+from employees.models import Employees
 
 
 class StudentSerializer(serializers.ModelSerializer):
     class Meta:
         model = Students
+        fields = "__all__"
+
+class EmoloyeeSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = Employees
         fields = "__all__"
