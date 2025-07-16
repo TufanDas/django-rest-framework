e46670123fb01d8d195c7a3923df7577961ef11d
Tufan <dtufan38@gmail.com>
2025-07-16 17:15:40 +0530

    Generic view , ListCreateApiView

---
+++ api/generic_views/generic_view.py
@@ -0,0 +1,21 @@
+from django.shortcuts import render  # (Optional) Used to render templates — not used in this API view
+from django.http import JsonResponse  # Useful for sending JSON responses — also not used here directly
+from students.models import Students  # Imports the Students model from the students app
+from ..serializers import StudentSerializer  # Imports the serializer to convert model instances to JSON
+from rest_framework.response import Response  # A DRF helper to send standard API responses
+from rest_framework import status  # Provides HTTP status codes for clarity
+from rest_framework.decorators import api_view  # Allows defining function-based views that support REST methods
+from rest_framework.views import APIView
+from employees.models import Employees
+from ..serializers import *
+from django.http import Http404
+from rest_framework import mixins, generics # Django REST framework mixins and base view
+
+
+# Generic view
+class Employees(generics.ListCreateAPIView):
+    # This query retrieves all Employee records from the database
+    queryset = Employees.objects.all()
+
+    # Specifies the serializer that converts model instances to JSON (and vice versa)
+    serializer_class = EmoloyeeSerializer
--- api/urls.py
+++ api/urls.py
@@ -1,6 +1,7 @@
 from django.urls import path  # Imports the path function to define URL patterns
 from . import views           # Imports views from the current app directory
-from api.view import mixin_views 
+from api.mixins_views import mixin_views 
+from api.generic_views import generic_view 
 
 # URL configuration for the app
 urlpatterns = [
@@ -14,5 +15,6 @@ urlpatterns = [
     path('mixins/employees/', mixin_views.Employeess.as_view()),
     path('mixins/employees/<int:pk>/', mixin_views.EmployeeDetail.as_view()),
 
-    path('generics/employees/', )
+    path('generic/employees/', generic_view.Employees.as_view()),
+    
 ]
